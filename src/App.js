926e3599e081c86e0cd1d2dc64228d0da2b4cb49
tubidge <tubidge@gmail.com>
2019-06-29 17:54:05 -0600

    adding goals view component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import ProfileView from "./components/ProfileView";
 import ProfileEdit from "./components/ProfileEdit";
+import GoalsView from "./components/GoalsView";
 import Footer from "./components/Footer";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <div>
       <Navbar />
       <ProfileEdit />
+      <hr />
+      <GoalsView />
       <Footer />
     </div>
   )
